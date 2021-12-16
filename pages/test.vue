95e5faa9213c4339bcd01e7d7293cf844088f140
유순조 <tnswh2023@phpschool.com>
2021-12-16 19:57:45 +0900

    first commit

---
+++ pages/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    hello {{name}}
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: "nuxt js"
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
