e652ca2ca057e4c6a301fb8ff0854fed5ae88f6f
YuSunjo <tnswh2023@gmail.com>
2021-12-26 11:26:48 +0900

    feat(default): 디폴트 파일

---
+++ blog-admin/component/loading/LoadingDialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-dialog
+      v-model="dialog"
+      hide-overlay
+      persistent
+      width="300"
+  >
+    <v-card
+        color="primary"
+        dark
+    >
+      <v-card-text>
+        잠시만 기다려주세요...
+        <v-progress-linear
+            indeterminate
+            color="white"
+            class="mb-0"
+        ></v-progress-linear>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  model: {
+    dialog: {
+      type: Boolean,
+      required: false,
+      default: false,
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- blog-admin/layouts/default.vue
+++ blog-admin/layouts/default.vue
@@ -1,83 +1,120 @@
 <template>
   <v-app>
-    <nav>
-      <v-app-bar
-        elevation='4'
-        shaped
-        color='#42A5F5'
-        dense
+    <LoadingDialog :dialog="true" />
+    <v-navigation-drawer
+        v-model="drawer"
+        :mini-variant="miniVariant"
+        clipped
+        app
+    >
+      <v-divider></v-divider>
+      <v-list>
+        <!-- <v-list-group
+        v-for="item in items"
+        :key="item.title"
+        v-model="item.active"
+        :prepend-icon="item.action"
+        no-action
+      > -->
+        <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            :to="item.to"
+            router
+            exact
+        >
+          <v-list-item-action>
+            <v-icon color="primary">{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+        clipped-left
+        app
+    >
+      <v-app-bar-nav-icon color="primary" @click.stop="drawer = !drawer"/>
+      <v-btn
+          icon
+          color="primary"
+          @click.stop="miniVariant = !miniVariant"
       >
-        <v-btn depressed text nuxt to='/'>
-          <v-icon dark>
-            mdi-home
-          </v-icon>
-        </v-btn>
-        <v-divider class='mx-4' vertical>
-        </v-divider>
-        <v-toolbar-title>
-          blog-admin
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
-          <v-text-field label='검색' prepend-icon='mdi-magnify' hide-details
-                        :style="{ display: 'flex', alignItems: 'center' }" />
-          <v-btn text nuxt to='/' :style="{ display: 'flex', alignItems: 'center' }">
-            <v-icon dark>mdi-account</v-icon>
-          </v-btn>
-        </v-toolbar-items>
-      </v-app-bar>
-    </nav>
-    <v-row>
-      <v-col cols='4'>
-        <v-card max-width='500' class='mx-auto'>
-          <v-list>
-            <v-list-item-group v-model='model'>
-              <v-list-item
-                v-for='(item, i) in items'
-                :key='i'
-              >
-                <v-list-item-content>
-                  <v-list-item-title v-text='item.content'></v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-card>
-      </v-col>
-      <v-col cols='16'>
-
-      </v-col>
-    </v-row>
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-breadcrumbs exact></v-breadcrumbs>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <!-- <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn> -->
+    </v-app-bar>
+    <v-main>
+      <v-container class="d-flex justify-center align-center">
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <!-- <v-navigation-drawer
+      v-model="rightDrawer"
+      right
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+    <Footer/>
   </v-app>
 </template>
 
 <script>
+import LoadingDialog from '../component/loading/LoadingDialog.vue'
 export default {
-  data() {
+  components: { LoadingDialog },
+  data () {
     return {
+      clipped: false,
+      drawer: false,
       items: [
         {
-          icon: "mdi-apps",
-          content: "profile",
-          to: "/profile",
+          icon: 'mdi-apps',
+          title: '통계페이지',
+          to: '/'
         },
         {
-          icon: "mdi-apps",
-          content: "app",
-          to: "/app",
+          icon: 'mdi-cog',
+          title: '프로필',
+          to: '/profile'
         },
         {
-          icon: "mdi-apps",
-          content: "board",
-          to: "/board",
+          icon: 'mdi-broadcast',
+          title: '게시글 목록',
+          to: '/board'
+        },
+        {
+          icon: 'mdi-store-check',
+          title: '글쓰기',
+          to: '/createBoard'
         },
       ],
-      model: 0,
-    };
-  },
-};
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: '나의 블로그 어드민 페이지',
+    }
+  }
+}
 </script>
-
-<style scoped>
-
-</style>
