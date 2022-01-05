bb3144be4b1c4e5e89e2692c3882cdcd9b241f21
유순조 <tnswh2023@phpschool.com>
2022-01-05 11:53:42 +0900

    feat(ckeditor): editor 기본 틀

--- blog-admin/store/index.js
+++ blog-admin/store/index.js
@@ -1,32 +1,43 @@
 export const state = () => ({
-    hello: 'vuex',
-    errorMessage: '',
-    backgroundImage: '',
-})
+  hello: "vuex",
+  errorMessage: "",
+  backgroundImage: "",
+});
 
 export const mutations = {
-    hello(state) {
-        state.hello = "hello world"
-    },
-    getConfig(state, payload) {
-        state.backgroundImage = payload.backgroundImage
-    }
-}
+  hello(state) {
+    state.hello = "hello world";
+  },
+  getConfig(state, payload) {
+    state.backgroundImage = payload?.backgroundImage;
+  },
+};
 
 export const actions = {
-    nuxtServerInit({commit, dispatch, state}, { req }) {
-        return dispatch('members/getMyInfo');
-    },
-    async getConfig({commit}) {
-        try {
-            let response = await this.$axios.get('/api/v1/admin/config', {
-                headers: {
-                    Authorization: this.$cookies.get("token")
-                }
-            });
-            commit('getConfig', response.data.data);
-        } catch (e) {
-            alert("설정 파일을 불러오는데 실패했습니다.");
-        }
+  nuxtServerInit({ commit, dispatch, state }, { req }) {
+    return dispatch("members/getMyInfo");
+  },
+  async getConfig({ commit }) {
+    try {
+      let response = await this.$axios.get("/api/v1/admin/config", {
+        headers: {
+          Authorization: this.$cookies.get("token"),
+        },
+      });
+      commit("getConfig", response.data.data);
+    } catch (e) {
+      console.log(e);
     }
-}
+  },
+  async createConfig({ commit }, payload) {
+    try {
+      await this.$axios.post("/api/v1/admin/config", payload, {
+        headers: {
+          Authorization: this.$cookies.get("token"),
+        },
+      });
+    } catch (e) {
+      console.log("설정파일 생성하는데 실패했습니다.");
+    }
+  },
+};
