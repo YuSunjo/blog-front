b9cf4fc12d338d9b0478d0ed4b7d1b89f3eb6aa9
YuSunjo <tnswh2023@gmail.com>
2022-01-15 12:51:09 +0900

    feat(nuxt): nuxt config 설정 및 npm i

---
+++ blog-user/nuxt.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  eslint: {
+    fix: true,
+  },
+  head: {
+    title: "blog",
+    meta: [{ name: "referrer", content: "no-referrer" }],
+  },
+  modules: ["@nuxtjs/vuetify", "@nuxtjs/axios", "cookie-universal-nuxt"],
+  vuetify: {},
+  axios: {
+    // browserBaseURL: "http://localhost:9000",
+    // baseURL: "http://localhost:9000",
+    browserBaseURL: "http://115.71.238.146:8080",
+    baseURL: "http://115.71.238.146:8080",
+  },
+};
