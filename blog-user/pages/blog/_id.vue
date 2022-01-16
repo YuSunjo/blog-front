5c789ce66fe63132151c5c73ea15c8a2b718ea3b
YuSunjo <tnswh2023@gmail.com>
2022-01-16 15:28:50 +0900

    feat(store): vuex 설정

---
+++ blog-user/pages/blog/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    블로그 상세페이지
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ blog-user/store/index.js
@@ -0,0 +1,25 @@
+export const state = () => ({
+
+});
+
+export const mutations = {
+
+};
+
+export const actions = {
+  // nuxtServerInit({ commit, dispatch, state }, { req }) {
+  //   return dispatch("members/getMyInfo");
+  // },
+  // async getConfig({ commit }) {
+  //   try {
+  //     let response = await this.$axios.get("/api/v1/admin/config", {
+  //       headers: {
+  //         Authorization: this.$cookies.get("token"),
+  //       },
+  //     });
+  //     commit("getConfig", response.data.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // },
+};
