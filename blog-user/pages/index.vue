f5f31bb73ea40c36a9d558190baf3d9607cfd838
YuSunjo <tnswh2023@gmail.com>
2022-01-16 14:28:06 +0900

    feat(page): 인덱스 페이지 마무리

--- blog-user/pages/index.vue
+++ blog-user/pages/index.vue
@@ -1,14 +1,42 @@
 <template>
   <div>
     <skill />
+    <hr class="pt-10"/>
+    <v-row>
+      <v-col cols="12" xs="12" md="3">
+        <board-card />
+      </v-col>
+      <v-col cols="12" xs="12" md="3">
+        <board-card />
+      </v-col>
+      <v-col cols="12" xs="12" md="3">
+        <board-card />
+      </v-col>
+      <v-col cols="12" xs="12" md="3">
+        <board-card />
+      </v-col>
+      <v-col cols="12" xs="12" md="3">
+        <board-card />
+      </v-col>
+      <v-col cols="12" xs="12" md="3">
+        <board-card />
+      </v-col>
+      <v-col cols="12" xs="12" md="3">
+        <board-card />
+      </v-col>
+      <v-col cols="12" xs="12" md="3">
+        <board-card />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 
 import Skill from '../components/skill';
+import BoardCard from '../components/BoardCard';
 export default {
-  components: {Skill}
+  components: {BoardCard, Skill}
 }
 </script>
 
