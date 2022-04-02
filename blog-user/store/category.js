5d7ba283b91ce7a2fba58370314c6ee6d0adb3fb
YuSunjo <tnswh2023@gmail.com>
2022-04-02 10:50:12 +0900

    feat(page): category list baseUrl에 추가

--- blog-user/store/category.js
+++ blog-user/store/category.js
@@ -10,7 +10,7 @@ export const mutations = {
 
 export const actions = {
   async retrieveCategory({ commit }) {
-    let response = await this.$axios.get("http://115.71.238.146:9000/api/v1/admin/category/list");
+    let response = await this.$axios.get("/api/v1/category/list");
     commit("retrieveCategory", response.data.data);
   },
 };
