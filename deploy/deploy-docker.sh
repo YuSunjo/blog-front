b03613ae39b5075c689ce5d15a63ec0efe7ec857
유순조 <tnswh2023@gmail.com>
2022-06-15 15:57:48 +0900

    fix(docker): Dockerfile 수정

--- deploy/deploy-docker.sh
+++ deploy/deploy-docker.sh
@@ -6,8 +6,8 @@ ADMIN_DOCKER_NAME=blog-front-admin
 
 cat ~/deploy-front/GIT-TOKEN.txt | docker login ghcr.io -u yusunjo --password-stdin
 
-docker stop $API_DOCKER_NAME && docker rm -fv hh-record && docker rmi -f $API_DOCKER_IMAGE:latest
+docker stop $API_DOCKER_NAME && docker rm -fv $API_DOCKER_NAME && docker rmi -f $API_DOCKER_IMAGE:latest
 docker run -d -p 3000:3000 --name $API_DOCKER_NAME --restart always $API_DOCKER_IMAGE:latest
 
-docker stop $ADMIN_DOCKER_NAME && docker rm -fv hh-record && docker rmi -f $ADMIN_DOCKER_IMAGE:latest
+docker stop $ADMIN_DOCKER_NAME && docker rm -fv $ADMIN_DOCKER_NAME && docker rmi -f $ADMIN_DOCKER_IMAGE:latest
 docker run -d -p 3030:3030 --name $ADMIN_DOCKER_NAME --restart always $ADMIN_DOCKER_IMAGE:latest
